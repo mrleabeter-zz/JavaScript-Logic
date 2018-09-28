19385accd64a5c8659c4fd83ef7dc27ba5dce582
Christopher Leabeter <christopherleabeter@gmail.com>
2018-09-28 01:25:27 -0400

    Info for question 3

---
+++ Question3.js
@@ -0,0 +1,4 @@
+// Write a function that converts HEX to RGB. Then Make that function auto-dect the formats so that if you enter HEX color format it returns RGB and if you enter RGB color format it returns HEX.
+
+const testHex = "#8cf0a0";
+const testRGB = "rgb(219, 43, 111)";
